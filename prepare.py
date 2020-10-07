a5c3be845d51f7155e2f1ef6f92b79e0884b453b
datastraine <anthony.straine@gmail.com>
2020-10-07 09:24:57 -0500

    updates prepare file to remove transactiondate

--- prepare.py
+++ prepare.py
@@ -57,7 +57,8 @@ def prepare_zillow():
     df = df[df.propertylandusetypeid.isin([261, 262, 273])]
     #  df = df.dropna()
     df = df[(df['bathroomcnt'] > 0) & (df['bathroomcnt'] > 0)]
-    
+    df.drop(column=['transactiondate'], inplace = True)
+
     return df
 
 
@@ -80,5 +81,6 @@ def prepare_zillow_mvp():
     # Drop properties that don't have a room or a restroom
     df = df[(df['bathroomcnt'] > 0) & (df['bathroomcnt'] > 0)]
     df['more_than_two_bath'] = (df.bathroomcnt > 2).astype('int')
+    df.drop(column=['transactiondate'], inplace = True)
 
     return df
